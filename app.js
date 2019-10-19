06d8df522b86df250456fbcb6ffb0254a7bad942
fidr <f1gp3a@gmail.com>
2019-10-19 23:21:09 +0000

    revert

--- app.js
+++ app.js
@@ -13,17 +13,30 @@ const fakeDb = {
     "hib" :8
 }
 
+//RESTFUL ROUTES
+app.get("/", function(req, res){
+    res.redirect("/vaccines");
+});
+
+//INDEX ROUTE
 app.get("/vaccines", (req, res) => {
     const allVaccines = Object.keys(fakeDb);
     console.log(allVaccines);
     res.send(allVaccines);
 });
 
+//SHOW ROUTE
 app.get("/vaccines/:id", (req, res) => {
     const vaccine = req.params.id;
     const val = fakeDb[vaccine];
     res.render("index.ejs", {val:val, vaccine:vaccine});
 });
+
+//EDIT ROUTE
+app.get("/edit"), (req, res) => {
+    res.send("edit");
+};
+
 
 app.listen(3000, () => {
     console.log("server started");
